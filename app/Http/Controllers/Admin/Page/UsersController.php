10928744594a6f74cfd1c42c842a10160a94369d
ilya <ilyabazhkov@gmail.com>
2024-07-26 02:44:37 +0300

    form bilder

--- app/Http/Controllers/Admin/Page/UsersController.php
+++ app/Http/Controllers/Admin/Page/UsersController.php
@@ -35,12 +35,12 @@ class UsersController extends Controller
     public function userEdit(Request $request)
     {
         $user = User::where('id','=',$request->id)->first();
+        echo "<pre>";
+        var_dump($user->toArray());
+        echo "</pre>";
+        die();
         if(!empty($user->data)){
             $user->data = json_decode($user->data,true);
-            echo "<pre>";
-            var_dump($user->data);
-            echo "</pre>";
-            die();
             if(!empty($user->data[1])){
                 $user->data = json_encode(array_merge(...$user->data));
                 $user->save();
