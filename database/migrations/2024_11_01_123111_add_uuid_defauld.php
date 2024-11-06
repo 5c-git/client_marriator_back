ce28f2e769042b07d80e8946a02e6726580a994a
ilya <ilyabazhkov@gmail.com>
2024-11-07 02:49:26 +0300

    form bilder

--- database/migrations/2024_11_01_123111_add_uuid_defauld.php
+++ database/migrations/2024_11_01_123111_add_uuid_defauld.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('uuid')->index()->nullable(true)->change();
+            $table->string('uuid')->nullable(true)->change();
         });
 
     }
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('uuid')->index()->change();
+            $table->string('uuid')->change();
         });
     }
 };
